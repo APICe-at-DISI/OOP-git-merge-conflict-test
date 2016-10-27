bed943fbdd6ba94e64197448e4754a529d984e88
Danilo Pianini <danilo.pianini@gmail.com>
2016-10-27 17:21:22 +0200

    Print author information

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public final class HelloWorld {
 
+	private static final String AUTHOR = "Danilo Pianini";
+
 	public static void main(final String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("This program has been realised by " + AUTHOR);
 	}
 
 }
