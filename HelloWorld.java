8e0f29c12e060f3bdc62540343eff3e473616d61
Danilo Pianini <danilo.pianini@gmail.com>
2016-10-27 17:19:05 +0200

    Change HelloWorld to print the number of available processors

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public final class HelloWorld {
 
 	public static void main(final String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
+	}
+
+	public static int procNumber() {
+		return Runtime.getRuntime().availableProcessors();
 	}
 
 }
