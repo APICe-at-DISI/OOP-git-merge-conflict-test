9c519e78afb8f2a3372b2975d086c94d25049da6
Danilo Pianini <danilo.pianini@unibo.it>
2026-09-16 10:57:10 +0200

    switch to Java 25 compact source file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,7 @@
-public final class HelloWorld {
+int procNumber() {
+	return Runtime.getRuntime().availableProcessors();
+}
 
-	public static void main(final String[] args) {
-		System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
-	}
-
-	public static int procNumber() {
-		return Runtime.getRuntime().availableProcessors();
-	}
-
+void main() {
+	System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
 }
